34362338c93c89914915132f28db6918b6f83483
Protyusha Ghosh <sg151308@gmail.com>
2026-02-08 01:01:01 +0530

    added program to add two numbers
    Fixed formatting issues in the first input prompt and added a new method to accept two numbers at once.

--- add_two_ number .c
+++ add_two_ number .c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 int a,b,x;
-  printf("enter first number);
+  printf("enter first number");
     scanf("%d",&a);
   printf("enter secound number ");
   scanf("%d",&b);
@@ -15,5 +15,23 @@ return 0;
 //enter the first number : 4
 //enter the secound number : 4
 //  x= 8
+
+
+//lets make a secound method how to appcet input together at a time  //
+// i'm  re-write the same code// 
+
+#include<stdio.h>
+int main(){
+int a,b,x;
+ printf("enter two number:");
+  scanf("%d,%d",&a,&b);
+  x=a+b;
+  printf("X=%d",x);
   
+
+return 0;
+}
+//output
+// enter two number : 4,4
+//x= 8
   
