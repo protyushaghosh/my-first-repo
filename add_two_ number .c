d36e1df693fc5ebcc4e929a7a5e5e6855a8117cc
Protyusha Ghosh <sg151308@gmail.com>
2026-02-08 00:47:10 +0530

    added program to add two numbers
    Fix formatting issues and correct spelling errors.

---
+++ add_two_ number .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int a,b,x;
+  printf("enter first number);
+    scanf("%d",&a);
+  printf("enter secound number ");
+  scanf("%d",&b);
+  x=a+b;
+  printf("X=%d",x);
+  
+
+return 0;
+}
+//output 
+//enter the first number : 4
+//enter the secound number : 4
+//  x= 8
+  
+  
